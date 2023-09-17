fcc966750bff54da7bcdb375bb6cf302b4208c92
muhammed akil <83995707+moudy-wold@users.noreply.github.com>
2023-09-17 22:32:26 +0300

    Update page.jsx

--- page.jsx
+++ page.jsx
@@ -1,103 +1,23 @@
-'use client';
-import React,{useRef} from 'react'
-import "./style.css";
-import { AiOutlineHome } from 'react-icons/ai';
-import { AiOutlineProfile } from 'react-icons/ai';
-import { AiFillSetting } from 'react-icons/ai';
-import { FcAbout } from 'react-icons/fc';
-import { AiFillContacts } from 'react-icons/ai';
-import { GoSignOut } from 'react-icons/go';
-import { AiFillFacebook } from 'react-icons/ai';
-import { AiFillTwitterCircle } from 'react-icons/ai';
-import { AiFillInstagram } from 'react-icons/ai';
-function SideBar() {
-
-const items=[
-  {
-  title:"Home",
-  url:"/",
-  icon:<AiOutlineHome />,
-},
-  {
-  title:"Profile",
-  url:"/",
-  icon:<AiOutlineProfile />,
-},
-{
-  title:" Setting",
-  url:"/",
-  icon:<AiFillSetting />,
-},
-{
-  title:"About",
-  url:"/",
-  icon:<FcAbout />,
-},
-{
-  title:"Contact US",
-  url:"/",
-  icon:<AiFillContacts />,
-},
-{
-  title:"Sign Out",
-  url:"/",
-  icon:<GoSignOut />,
-},
-]
-const SocialIcons=[
-  {
-  title:"FaceBook",
-  url:"/",
-  icon:<AiFillFacebook />
-
-},
-{
-  title:"Twitter",
-  url:"/",
-  icon:<AiFillTwitterCircle />
-},
-{
-  title:"Insagram",
-  url:"/",
-  icon:<AiFillInstagram />
-},
-]
-const sidbar = useRef();
-
-// Handle Open Sidebar
-const handleOpenClose=()=>{
-  sidbar.current.classList.toggle("open");
-}
-
-  return (
-    <div className="parent">    
-
-      <div className="sidebar" ref={sidbar}>
-
-            {/* Icon For Open/close Sidebar */}
-            <span onClick={()=> handleOpenClose()}className="open-close"><AiFillSetting /> </span>
-
-            {/* Title */}
-            <h1>Moudy Wold</h1>
-
-            {/* Linke */}
-            <ul>
-               {items.map(item=>(        
-                  <li><a href={`${item.url}`}>{item.icon} <span>{item.title}</span></a></li>          
-                ))} 
-            </ul>
-
-            {/* Social Icons */}
-            <div className="social-icons">
-                {SocialIcons.map(item=>(
-                  <>
-                      <a href="#">{item.icon}</a>
-                  </>
-                ))}
-            </div>
-      </div>
-     </div>
-  )
-}
-
-export default SideBar
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <script src="https://kit.fontawesome.com/5160667314.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="parent">    
+        <div class="sidebar">
+            <span class="open-close"><i class="fa-solid fa-gear"></i></span>
+              <h1>Moudy Wold</h1>
+              <ul>              
+              </ul>
+              <div class="social-icons">                                  
+              </div>
+        </div>
+       </div>
+  <script src="./index.js"></script>
+</body>
+</html>
